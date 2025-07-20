2a4484c697a5c1566ddbd9c2a86b6abc781a4d1f
siriusmart <siriusmart@tuta.io>
2025-07-20 22:54:37 +0100

    minor fix

--- prelude.js
+++ prelude.js
@@ -1,4 +1,4 @@
 prelude.eval("let {removeEventListener} = module.globals.listener");
 prelude.eval(
-    "function addEventListener(a, b, o = {}) { o.source ??= module.path[1]; module.globals.listener.addEventListener(a, b, o) }",
+    "function addEventListener(a, b, o = {}) { o.source ??= module.path[1]; return module.globals.listener.addEventListener(a, b, o) }",
 );
